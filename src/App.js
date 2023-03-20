89e237af627443dbd8ba72d054de501f91d147c6
Franzi1005 <fschallhorn@gmail.com>
2023-03-20 18:59:45 +0100

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Franzi's React Weather App</h1>
-      <footer>
-        This site was coded by Franziska Schallhorn and is{" "}
-        <a
-          href="https://github.com/Franzi1005/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Franzi's React Weather App</h1>
+
+        <Weather />
+        <footer>
+          This site was coded by Franziska Schallhorn and is{" "}
+          <a
+            href="https://github.com/Franzi1005/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
