0ed44489756c3fc782d4c9f24cb0b2c6f1793974
Franzi1005 <fschallhorn@gmail.com>
2023-03-20 18:27:31 +0100

    Added GitHub Repo link

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Franzi's React Weather App</h1>
+      <footer>
+        This site was coded by Franziska Schallhorn and is{" "}
+        <a
+          href="https://github.com/Franzi1005/react-weather-app-project"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
